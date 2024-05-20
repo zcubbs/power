c2760ce832ca2d84d74e8a9bde14ea5bb664083e
zcubbs <zackcubby@gmail.com>
2024-05-20 20:12:21 +0200

    :zap: fix api url resolution

--- web/start/src/api.ts
+++ web/start/src/api.ts
@@ -16,7 +16,7 @@ type FetchBlueprintsResponse = {
 
 export const fetchBlueprints = async (): Promise<Blueprint[]> => {
   try {
-    const response: AxiosResponse<FetchBlueprintsResponse> = await axios.get(`${API_BASE_URL}/v1/blueprints`);
+    const response: AxiosResponse<FetchBlueprintsResponse> = await axios.get(`/v1/blueprints`);
     return response.data.blueprints;
   } catch (error) {
     console.error('Error fetching blueprints:', error);
@@ -36,7 +36,7 @@ type GenerateResponse = {
 
 export const generateBlueprint = async (blueprintId: string, values: Record<string, any>): Promise<string> => {
   try {
-    const response: AxiosResponse<GenerateResponse> = await axios.post(`${API_BASE_URL}/v1/generate`, {
+    const response: AxiosResponse<GenerateResponse> = await axios.post(`/v1/generate`, {
       blueprintId,
       values
     });
